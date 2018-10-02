c3437e7d326c7b09f8421b2a7bce7bfc1199dbe6
georgipetkov18 <43476005+georgipetkov18@users.noreply.github.com>
2018-10-02 22:13:26 +0300

    Add files via upload

---
+++ zad/zad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetMax();
+        }
+        public static void GetMax()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+            }
+            if (b > a && b > c)
+            {
+                Console.WriteLine(b);
+            }
+            if(c > a && c > b)
+            {
+                Console.WriteLine(c);
+            }
+            
+        }
+    }
+}
